3b848298b955883302ca3c27eec34050dc7fea9e
Weinan Qiu <davidiamyou@gmail.com>
2018-11-10 14:26:39 -0500

    DefaultAccessProviderSpec encode response spec

--- src/test/kotlin/io/imulab/astrea/provider/DefaultAccessProviderSpec.kt
+++ src/test/kotlin/io/imulab/astrea/provider/DefaultAccessProviderSpec.kt
@@ -1,6 +1,9 @@
 package io.imulab.astrea.provider
 
+import io.imulab.astrea.domain.*
 import io.imulab.astrea.domain.request.AccessRequest
+import io.imulab.astrea.domain.response.AccessResponse
+import io.imulab.astrea.domain.response.impl.DefaultAccessResponse
 import io.imulab.astrea.error.OAuthException
 import io.imulab.astrea.support.HttpSupport
 import io.imulab.astrea.support.ProviderSupport
@@ -9,8 +12,43 @@ import org.assertj.core.api.Assertions.*
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.lang.RuntimeException
+import java.time.LocalDateTime
 
 object DefaultAccessProviderSpec : Spek({
+
+    describe("write response") {
+        val provider: AccessProvider = ProviderSupport.Access.forTestJsonCapability()
+        val request: AccessRequest = RequestSupport.newAccessRequest()
+
+        it("""
+            encode response
+        """.trimIndent()) {
+            val response: AccessResponse = DefaultAccessResponse().also {
+                it.setAccessToken("test-access-token")
+                it.setExpiry(LocalDateTime.now().plusDays(1))
+                it.setScopes(listOf("foo", "bar"))
+                it.setTokenType(TokenType.Bearer)
+                it.setExtra("refresh_token", "test-refresh-token")
+            }
+            val writer: HttpSupport.MapHttpResponseWriter = HttpSupport.response()
+
+            assertThatCode {
+                provider.encodeAccessResponse(writer, request, response)
+            }.doesNotThrowAnyException()
+
+            writer.assertStatus(200)
+            writer.assertHeader("Content-Type", "application/json;charset=UTF-8")
+            writer.assertHeader("Pragma", "no-cache")
+            writer.assertHeader("Cache-Control", "no-store")
+            writer.getBodyAsJsonMap().run {
+                assertThat(this[PARAM_ACCESS_TOKEN]).isNotNull().asString().isEqualTo("test-access-token")
+                assertThat(this[PARAM_TOKEN_TYPE]).isNotNull().asString().isEqualTo(TokenType.Bearer.specValue)
+                assertThat(this[PARAM_REFRESH_TOKEN]).isNotNull().asString().isEqualTo("test-refresh-token")
+                assertThat(this[PARAM_SCOPE]).isNotNull().asString().contains("foo", "bar")
+                return@run
+            }
+        }
+    }
 
     describe("write error") {
 
