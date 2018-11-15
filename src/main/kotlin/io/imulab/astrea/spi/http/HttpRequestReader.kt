2571f3a2acad867d6cfcbf55d6845905c2731d25
Weinan Qiu <davidiamyou@gmail.com>
2018-11-14 21:31:47 -0500

    remove deprecated spi method declaration

--- src/main/kotlin/io/imulab/astrea/spi/http/HttpRequestReader.kt
+++ src/main/kotlin/io/imulab/astrea/spi/http/HttpRequestReader.kt
@@ -11,18 +11,6 @@ interface HttpRequestReader {
     fun method(): String
 
     /**
-     * Get the **first** corresponding value of [key] in the http form. Returns empty
-     * string if not found.
-     */
-    @Deprecated("use HttpRequestReader.getForm().mustSingleValue(key) to enforce single value.",
-            level = DeprecationLevel.ERROR)
-    fun formValue(key: String): String
-
-    @Deprecated("use HttpRequestReader.getForm().mustSingleValue(key) to enforce single value.",
-            level = DeprecationLevel.ERROR)
-    fun formValueUnescaped(key: String): String
-
-    /**
      * Return the http form.
      */
     fun getForm(): UrlValues
